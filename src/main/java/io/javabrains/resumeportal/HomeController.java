bad462cd445f30dcf06f0bc41bde4a873ebc9ca4
Exequiel <exe.pine@gmail.com>
2021-10-02 20:10:39 -0300

    Added Thymeleaf

--- src/main/java/io/javabrains/resumeportal/HomeController.java
+++ src/main/java/io/javabrains/resumeportal/HomeController.java
@@ -1,18 +1,26 @@
 package io.javabrains.resumeportal;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "hello";
     }
 
     @GetMapping("/edit")
-    public String edit(){
+    public String edit() {
         return "edit page";
+    }
+
+    @GetMapping("/view/{userId}")
+    public String view(@PathVariable("userId") String userId, Model model){
+        model.addAttribute("userId",userId);
+        return "profile";
     }
 }
