e9af651543e03259634c42d628a0fd03e8b0e025
Exequiel <exe.pine@gmail.com>
2021-10-03 18:14:03 -0300

    Added job entity and some hard coded examples for testing

--- src/main/java/io/javabrains/resumeportal/HomeController.java
+++ src/main/java/io/javabrains/resumeportal/HomeController.java
@@ -1,16 +1,16 @@
 package io.javabrains.resumeportal;
 
-import io.javabrains.resumeportal.models.User;
+import io.javabrains.resumeportal.models.Job;
 import io.javabrains.resumeportal.models.UserProfile;
 import io.javabrains.resumeportal.repository.UserProfileRepository;
-import io.javabrains.resumeportal.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Optional;
 
 @Controller
@@ -21,7 +21,35 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "hello";
+
+        UserProfile profile1 = new UserProfile();
+        profile1.setId(1);
+        profile1.setUserName("einstein");
+        profile1.setFirstName("Albert");
+        profile1.setLastName("Einstein");
+        profile1.setDesignation("Fisico teorico");
+        profile1.setTheme(1);
+        Job job1 = new Job();
+        job1.setCompany("Company 1");
+        job1.setDesignation("Designation 1");
+        job1.setId(1);
+        job1.setStartDate(LocalDate.of(2020,1,1));
+        job1.setEndDate(LocalDate.of(2020,3,1));
+
+        Job job2 = new Job();
+        job2.setCompany("Company 2");
+        job2.setDesignation("Designation 1");
+        job2.setId(2);
+        job2.setStartDate(LocalDate.of(2019,5,1));
+        job2.setEndDate(LocalDate.of(2020,1,1));
+
+        profile1.setJobs(Arrays.asList(job1,job2));
+
+        userProfileRepository.save(profile1);
+
+        System.out.println(profile1.getJobs());
+
+        return "profile";
     }
 
     @GetMapping("/edit")
