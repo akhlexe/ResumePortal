517607421a697b5fecd47620edd7bf1dc2520306
Exequiel <exe.pine@gmail.com>
2021-11-14 18:00:12 -0300

    Adding skills to the edit profile

--- src/main/java/io/javabrains/resumeportal/models/Education.java
+++ src/main/java/io/javabrains/resumeportal/models/Education.java
@@ -1,9 +1,6 @@
 package io.javabrains.resumeportal.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
